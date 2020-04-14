7028e82b49896682b1ece34d67a23bbd291e75c6
Elliot Blanchard <public@elliotblanchard.com>
2020-04-14 14:02:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,9 +20,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   holiday_hash.each do |season, season_hash|
     if season == :summer
+      binding.pry
       season_hash.each do |holiday, holiday_array|
         if holiday == :fourth_of_july
-          binding.pry
           return holiday_array[1]
         end
       end
